49ce5043553853b5e617cc8b5c645dcdd80b19da
Soichi Hayashi <hayashis@iu.edu>
2017-08-02 21:58:39 +0000

    switched the startlxde/x11vnc

--- vnc.sh
+++ vnc.sh
@@ -9,15 +9,13 @@ while ! xdpyinfo -display :0; do
     sleep 0.2
 done
 
+echo "-----------------------------------lxde-----------------------------"
+#I think I need to start the lxde before I can start x11?
+DISPLAY=:0 startlxde &
+
 echo "-----------------------------------x11vnc-----------------------------"
 #TODO - -xrandr displays "Disabling -xrandr mode: display does not support X RANDR."
 #I think something needs to be compiled into x11vnc for it to work?
-x11vnc -forever -display :0 -passwd -ncache_cr $X11VNC_PASSWORD &
-
-#not necessary?
-sleep 0.2
-
-echo "-----------------------------------lxde-----------------------------"
-#DISPLAY=:0 startlxde
+x11vnc -forever -display :0 -passwd -ncache_cr $X11VNC_PASSWORD
 
 
