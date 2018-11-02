20144e203fa35bda6be1d663fc3f0bfbecf071c0
Soichi Hayashi <hayashis@iu.edu>
2018-11-01 20:31:59 -0400

    updated build.sh to push all tags

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ docker pull neurodebian:nd16.04
 
 docker build -t soichih/vncserver-fslview .
 docker tag soichih/vncserver-fslview soichih/vncserver-fslview:$tag
-docker push soichih/vncserver-fslview:$tag
+docker push soichih/vncserver-fslview
